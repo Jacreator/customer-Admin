b9a263f3b8018060882d808b038ea660b9c0f450
Jacreator <jambone.james82@gmail.com>
2020-01-16 13:00:14 +0100

    filter file corrected

--- customerAdmin/customerAdminApp/views.py
+++ customerAdmin/customerAdminApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
-from .models import Order, Customer
+from .models import *
 from .forms import OrderForm
 from .filters import OrderFilter
 
